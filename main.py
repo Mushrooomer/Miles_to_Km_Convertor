d6dc235cca9b5192141d8ad9e2c9b42b45b47a83
Recepce <k.klempir@gmail.com>
2022-10-13 11:43:11 +0200

    test 2

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("My first GUI program")
+window.minsize(width=500, height=500)
+
+#Label
+
+my_label = tkinter.Label(text="I am a label")
+my_label.pack()
+
+
+
 
 
 
