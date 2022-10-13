1868c20014f215777abbab812abe821013c14ba1
Recepce <k.klempir@gmail.com>
2022-10-13 17:42:23 +0200

    test 3

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=500)
 
-#Label
-
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
 my_label.pack()
 
-import turtle
 
-tim = turtle.Turtle()
-tim.write()
+button = Button(text="Click Me!", command=button_clicked)
+button.pack()
 
-
-
-
-
-
+# Entry
+input = Entry(width=10)
+print(input.get())
+input.pack()
 
 
-
-
-
-
-
-
+#def button_clicked():
+   # print("I got clicked")
+   # new_text = input.get()
+  #  my_label.config(text=new_text)
 
 
 
 window.mainloop()
-
